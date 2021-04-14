0d2c80b53669b568f4144ff0f839bf914aa7d3a1
hadieyhaliemy <hadieyhaliemy>
2021-04-14 15:35:26 +0900

    Week06 (post-merge final edits)

--- test/src/testpackage/Searcher.java
+++ test/src/testpackage/Searcher.java
@@ -1,12 +1,8 @@
 package testpackage;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.ObjectInputStream;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -45,83 +41,42 @@ public class Searcher {
 
  			  
  			  for(String id : output) {
- 				  
- 				  if(Character.getNumericValue(id.charAt(0))==1) {
+ 				  for (int m = 1; m<=5;m++) {
+ 					   if(Character.getNumericValue(id.charAt(0))==m) {
  					  
  					 int wq =kwrd.getCnt();
  					 String[] tfidf = id.split(",");
  					 double weight = Double.parseDouble(tfidf[1]);
  					 double sum = weight*wq;
- 					 Freq[0][0] += sum;
- 					 Freq[0][1] += wq*wq;
- 					 Freq[0][2] += weight*weight;
- 					 
- 				  }else if(Character.getNumericValue(id.charAt(0))==2){
- 	
- 					  int wq =kwrd.getCnt();
-					  String[] tfidf = id.split(",");
-					  double weight = Double.parseDouble(tfidf[1]);
-					  double sum = weight*wq;
-
-					  Freq[1][0] += sum;
-					  Freq[1][1] += wq*wq;
-					  Freq[1][2] += weight*weight;
-					  
- 				  }else if(Character.getNumericValue(id.charAt(0))==3){
- 				
- 					 int wq =kwrd.getCnt();
-					  String[] tfidf = id.split(",");
-					  double weight = Double.parseDouble(tfidf[1]);
-					  double sum = weight*wq;
-
-					  Freq[2][0] += sum;
-					  Freq[2][1] += wq*wq;
-					  Freq[2][2] += weight*weight;
-				
- 				  }else if(Character.getNumericValue(id.charAt(0))==4) {
- 					 int wq =kwrd.getCnt();
-					  String[] tfidf = id.split(",");
-					  double weight = Double.parseDouble(tfidf[1]);
-					  double sum = weight*wq;
-
-					  Freq[3][0] += sum;
-					  Freq[3][1] += wq*wq;
-					  Freq[3][2] += weight*weight;
-					
- 				  }else {
- 					 int wq =kwrd.getCnt();
-					  String[] tfidf = id.split(",");
-					  double weight = Double.parseDouble(tfidf[1]);
-					  double sum = weight*wq;
-
-					  Freq[4][0] += sum;
-					  Freq[4][1] += wq*wq;
-					  Freq[4][2] += weight*weight;
-			
+ 					 Freq[m-1][0] += sum;
+ 					 Freq[m-1][1] += wq*wq;
+ 					 Freq[m-1][2] += weight*weight;
+ 					   }
  				  }
+ 				  
  			  }
+ 			  
  			  break;
+ 			  
  		   }else 
  			  continue;
  			  }
  		   }
     return Freq;
  	   }
-    
  		   
 	
 	public void CalcSim (String input, double[][]Freq) throws IOException, ClassNotFoundException {
 		
-		
         double [][] Frequency = new double[5][2];
         for(int i = 0; i<Frequency.length; i++) {
+        	
         	Frequency[i][0] = i+1;
-//        	System.out.println(Freq[i][1]+" "+Freq[i][2]);
         	Frequency[i][1] = Freq[i][0]/(Math.sqrt(Freq[i][1])*Math.sqrt(Freq[i][2]));
-//        	System.out.println(Frequency[i][1]);
 
         }
         java.util.Arrays.sort(Frequency, new java.util.Comparator<double[]>() {
+        	
             public int compare(double[] a, double[] b) {
                 return Double.compare(a[1], b[1]);
             }
@@ -131,22 +86,22 @@ public class Searcher {
        String[] rank = new String[3];
        String[] doctest = {"떡 ","초밥 ","파스타 ","아이스크림 ","라면 "};
        int count=0;
+       
        for (int i = 0; i<3;i++) {
-
     	   for(int j = 0; j < doctest.length;j++) {
+    		   
     			   if(Frequency[i][0] == j+1)
     				  rank[count++] = doctest[j];
     		   
     	   }
        }
-
-         
-  
+       
         System.out.println("Ranking 1: "+rank[0]+" / Weight : "+Frequency[0][1]+"\nRanking 2: "+rank[1]+" / Weight : "+Frequency[1][1]+"\nRanking 3: "+rank[2]+" / Weight : "+Frequency[2][1]);
 
 	}
 	
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Please Input Text:");
 		String input = sc.nextLine();
